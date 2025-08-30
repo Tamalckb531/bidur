86dacacfaa8040b66a3b181bbe8364cb3052057b
Tamal <ckbtamaldipnew@gmail.com>
2025-08-31 00:51:14 +0600

    extension -> add background.ts pagetype wise event register

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,1 +1,54 @@
-console.log("Hi there from background");
+import { getGitHubPageType } from "../helpers/func";
+import { ChromeTypes } from "../types/data.type";
+
+console.log("Background is running");
+
+chrome.runtime.onMessage.addListener(
+  async (message, _sender, _sendResponse) => {
+    if (message.type === ChromeTypes.INIT) {
+      const { url, tabId } = message;
+
+      const pageType = getGitHubPageType(url);
+      console.log(
+        "Getting page type from the init background end : ",
+        pageType
+      );
+
+      // let storable: boolean = false;
+
+      // if (pageType !== "NONE") {
+      //   storable = await isDataStorable(url);
+      //   console.log(
+      //     "Background -> is the data storable in the vector database ? Answer: ",
+      //     storable
+      //   );
+      // }
+
+      // if (!storable) return;
+
+      //? page type wise event register for the content.
+      switch (pageType) {
+        case "PROFILE":
+          chrome.tabs.sendMessage(tabId, { type: ChromeTypes.PROFILE });
+          break;
+        case "REPO":
+          chrome.tabs.sendMessage(tabId, { type: ChromeTypes.REPO });
+          break;
+        case "REPO_IN_Folder":
+          chrome.tabs.sendMessage(tabId, {
+            type: ChromeTypes.REPO_FOLDER,
+            url: url,
+          });
+          break;
+        case "REPO_IN_File":
+          chrome.tabs.sendMessage(tabId, {
+            type: ChromeTypes.REPO_FILE,
+            url: url,
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+);
--- extension/src/helpers/func.ts
+++ extension/src/helpers/func.ts
@@ -1,3 +1,45 @@
+import type { UrlType } from "../types/data.type";
+
+const reservedPages = [
+  "dashboard",
+  "notifications",
+  "settings",
+  "pulls",
+  "issues",
+  "marketplace",
+  "explore",
+  "apps",
+  "topics",
+  "collections",
+  "events",
+  "sponsors",
+  "codespaces",
+  "copilot",
+  "discussion",
+  "projects",
+];
+
 export const isGithubUrl = (url: string): boolean => {
   return url.includes("github.com");
+};
+export const getGitHubPageType = (url: string): UrlType => {
+  try {
+    const parsed = new URL(url);
+    if (parsed.hostname !== "github.com") return "NONE";
+
+    const pathParts = parsed.pathname.split("/").filter(Boolean);
+
+    if (pathParts.length == 0) return "NONE";
+    if (reservedPages.includes(pathParts[0])) return "NONE";
+
+    if (pathParts.length === 1) return "PROFILE";
+    if (pathParts.length === 2) return "REPO";
+    if (pathParts.length >= 3 && pathParts[2] === "tree")
+      return "REPO_IN_Folder";
+    if (pathParts.length >= 3 && pathParts[2] === "blob") return "REPO_IN_File";
+
+    return "NONE";
+  } catch {
+    return "NONE";
+  }
 };
