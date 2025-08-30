c7822dcdb3ac3d3f59022da2361e6613ae5f23ab
Tamal <ckbtamaldipnew@gmail.com>
2025-08-30 13:54:35 +0600

    server -> add signup controller

---
+++ server/src/controller/auth.controller.ts
@@ -0,0 +1,74 @@
+import { Context } from "hono";
+import {
+  LoginBodyTypes,
+  LoginSchema,
+  SignUpBodyTypes,
+  SignUpSchema,
+} from "../types/schema/zod.index";
+import { HTTPException } from "hono/http-exception";
+import bcrypt from "bcryptjs";
+import { encryptApiKey } from "../libs/encryptions";
+import jwt from "jsonwebtoken";
+import { User } from "../generated/prisma";
+import { getPrisma } from "../libs/prismaFunc";
+
+export const signup = async (c: Context) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+
+  const { email, password, name, apiKey } = await c.req.json<SignUpBodyTypes>();
+
+  console.log("Payload from extension : ", email, password, name, apiKey);
+
+  try {
+    SignUpSchema.parse({ name, email, apiKey, password });
+
+    const isUserExist: boolean =
+      (await prisma.user.count({
+        where: { email },
+      })) > 0;
+
+    if (isUserExist) {
+      throw new HTTPException(400, { message: "This email already exist" });
+    }
+
+    const hashedPassword: string = bcrypt.hashSync(password, 10);
+
+    const encryptedApiKey: string = encryptApiKey(apiKey, c.env.ENCRYPTION_KEY);
+
+    const newUser: User = await prisma.user.create({
+      data: {
+        email,
+        name,
+        apiKey: encryptedApiKey,
+        password: hashedPassword,
+      },
+    });
+
+    if (!c.env.JWT_SECRET_KEY) {
+      throw new Error(
+        "JWT_SECRET_KEY is not defined in the environment variables"
+      );
+    }
+
+    const token: string = jwt.sign(
+      {
+        id: newUser.id,
+      },
+      c.env.JWT_SECRET_KEY
+    );
+
+    const { password: pass, apiKey: key, ...user } = newUser;
+
+    console.log("Signup successful for user : ", user);
+
+    return c.json({
+      msg: "Signed up successfully",
+      user: user,
+      token,
+    });
+  } catch (e: any) {
+    throw new HTTPException(500, {
+      message: e.message || "An error occurred while logging in",
+    });
+  }
+};
